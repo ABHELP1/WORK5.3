1943ec89f7e23435fc25ce3d1308ef5410fb62fe
channasorn chungmarisakul <63010185@KMITL.AC.TH>
2020-09-13 20:26:51 +0700

    Add project files.

---
+++ WORK5.3/Source.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int a[15], b[50];
+int main() {
+    int i, ans = 0, m;
+    for (i = 0; i < 10; i++) {
+        scanf("%d", &a[i]);
+        m = a[i] % 42;
+        b[m] = 1;
+    }
+    for (i = 0; i < 42; i++) {
+        if (b[i] == 1)
+            ans++;
+    }
+    printf("%d", ans);
+}
